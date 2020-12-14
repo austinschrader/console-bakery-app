1653725a0095802a12338d63bf5e18edd5703144
Austin Schrader <austin.jschrader@gmail.com>
2020-12-13 23:19:38 -0800

    Add some UI to take in breadCount and pastryCount

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -6,9 +6,15 @@ namespace Bakery
   {
     static void Main()
     {
-      Console.WriteLine("hello world");
-      Console.WriteLine("What would you like to purchase?");
-      string userInput = Console.ReadLine();
+      Console.WriteLine("Welcome. The cost for bread is as follows:");
+      Console.WriteLine("For 1 loaf, it's $5. If you buy 2, then you get the 3rd for free.");
+      Console.WriteLine("For pastries you can buy 1 for $2, or 3 for $5.");
+      Console.WriteLine("How many loaves of bread would you like?");
+      string breadCount = Console.ReadLine();
+      Console.WriteLine("How many pastries would you like?");
+      string pastryCount = Console.ReadLine();
+
+
 
     }
   }
