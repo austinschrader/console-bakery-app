d3f920046e6bb465e8f164c529d98cb1b02d7262
Austin Schrader <austin.jschrader@gmail.com>
2020-12-16 20:13:23 -0800

    Add additional bread tests

--- Bakery.Tests/Model.Tests/BreadTests.cs
+++ Bakery.Tests/Model.Tests/BreadTests.cs
@@ -16,11 +16,56 @@ namespace Bakery.Tests
     }
 
     [TestMethod]
-    public void BreadOrderCalc_ReturnsBreadTotal_Int()
+    public void BreadOrderCalc_ReturnsBreadTotalForZeroLoaves_Int()
+    {
+      Bread newBread = new Bread("yummy");
+      int breadCount = 0;
+      int breadCost = 0;
+      Assert.AreEqual(breadCost, newBread.BreadOrderCalc(breadCount));
+    }
+
+    [TestMethod]
+    public void BreadOrderCalc_ReturnsBreadTotalForTwoLoaves_Int()
+    {
+      Bread newBread = new Bread("yummy");
+      int breadCount = 2;
+      int breadCost = 10;
+      Assert.AreEqual(breadCost, newBread.BreadOrderCalc(breadCount));
+    }
+
+    [TestMethod]
+    public void BreadOrderCalc_ReturnsBreadTotalForThreeLoaves_Int()
     {
       Bread newBread = new Bread("yummy");
       int breadCount = 3;
+      int breadCost = 10;
+      Assert.AreEqual(breadCost, newBread.BreadOrderCalc(breadCount));
+    }
+
+    [TestMethod]
+    public void BreadOrderCalc_ReturnsBreadTotalForFourLoaves_Int()
+    {
+      Bread newBread = new Bread("yummy");
+      int breadCount = 4;
       int breadCost = 15;
+      Assert.AreEqual(breadCost, newBread.BreadOrderCalc(breadCount));
+    }
+
+    [TestMethod]
+    public void BreadOrderCalc_ReturnsBreadTotalForFiveLoaves_Int()
+    {
+      Bread newBread = new Bread("yummy");
+      int breadCount = 5;
+      int breadCost = 20;
+      Assert.AreEqual(breadCost, newBread.BreadOrderCalc(breadCount));
+    }
+
+    [TestMethod]
+    public void BreadOrderCalc_ReturnsBreadTotalForSixLoaves_Int()
+    {
+      Bread newBread = new Bread("yummy");
+      int breadCount = 6;
+      int breadCost = 20;
       Assert.AreEqual(breadCost, newBread.BreadOrderCalc(breadCount));
     }
 
