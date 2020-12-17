1bb7190fa77ed6c6004d7e5314ef4f527c8c9cb2
Austin Schrader <austin.jschrader@gmail.com>
2020-12-16 20:22:32 -0800

    Add additional pastry model tests

--- Bakery.Tests/Model.Tests/PastryTests.cs
+++ Bakery.Tests/Model.Tests/PastryTests.cs
@@ -17,11 +17,65 @@ namespace Bakery.Tests
     }
 
     [TestMethod]
-    public void PastryOrderCalc_ReturnsPastryTotal_Int()
+    public void PastryOrderCalc_ReturnsPastryTotalForZeroPastries_Int()
+    {
+      Pastry newPastry = new Pastry("yummy");
+      int pastryCount = 0;
+      int pastryCost = 0;
+      Assert.AreEqual(pastryCost, newPastry.PastryOrderCalc(pastryCount));
+    }
+
+    [TestMethod]
+    public void PastryOrderCalc_ReturnsPastryTotalForOnePastry_Int()
+    {
+      Pastry newPastry = new Pastry("yummy");
+      int pastryCount = 1;
+      int pastryCost = 2;
+      Assert.AreEqual(pastryCost, newPastry.PastryOrderCalc(pastryCount));
+    }
+
+    [TestMethod]
+    public void PastryOrderCalc_ReturnsPastryTotalForTwoPastries_Int()
+    {
+      Pastry newPastry = new Pastry("yummy");
+      int pastryCount = 2;
+      int pastryCost = 4;
+      Assert.AreEqual(pastryCost, newPastry.PastryOrderCalc(pastryCount));
+    }
+
+    [TestMethod]
+    public void PastryOrderCalc_ReturnsPastryTotalForThreePastries_Int()
     {
       Pastry newPastry = new Pastry("yummy");
       int pastryCount = 3;
       int pastryCost = 5;
+      Assert.AreEqual(pastryCost, newPastry.PastryOrderCalc(pastryCount));
+    }
+
+    [TestMethod]
+    public void PastryOrderCalc_ReturnsPastryTotalForFourPastries_Int()
+    {
+      Pastry newPastry = new Pastry("yummy");
+      int pastryCount = 4;
+      int pastryCost = 7;
+      Assert.AreEqual(pastryCost, newPastry.PastryOrderCalc(pastryCount));
+    }
+
+    [TestMethod]
+    public void PastryOrderCalc_ReturnsPastryTotalForFivePastries_Int()
+    {
+      Pastry newPastry = new Pastry("yummy");
+      int pastryCount = 6;
+      int pastryCost = 10;
+      Assert.AreEqual(pastryCost, newPastry.PastryOrderCalc(pastryCount));
+    }
+
+    [TestMethod]
+    public void PastryOrderCalc_ReturnsPastryTotalForSixPastries_Int()
+    {
+      Pastry newPastry = new Pastry("yummy");
+      int pastryCount = 6;
+      int pastryCost = 10;
       Assert.AreEqual(pastryCost, newPastry.PastryOrderCalc(pastryCount));
     }
 
